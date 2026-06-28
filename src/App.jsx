c5ac3f16ab2bc43e6d624f61d4c07189a951a3b9
M. Hanif Irfan <mhirfan173@gmail.com>
2026-06-28 21:17:31 +0700

    feat: add framework setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-linear-to-r from-blue-400 to-emerald-400">
+        Portofolio Saya Segera Hadir
+      </h1>
+    </div>
+  )
+}
+
+export default App
